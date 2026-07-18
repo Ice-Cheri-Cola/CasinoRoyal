bdd7919913588bd4c302b72f057eba06dd7b6fcc
Ice-Cheri-Cola <alyshamariedel@gmail.com>
2026-07-17 22:36:00 -0400

    Create themes.lua

---
+++ assets/themes.lua
@@ -0,0 +1,57 @@
+--================================================--
+-- Casino Royal
+-- Version: 0.2.0
+-- File: assets/themes.lua
+-- Description: Casino visual themes
+--================================================--
+
+local themes = {}
+
+
+themes.ATM10 = {
+
+    name = "ATM10",
+
+    background =
+        colors.black,
+
+    primary =
+        colors.purple,
+
+    secondary =
+        colors.cyan,
+
+    accent =
+        colors.lightBlue,
+
+    text =
+        colors.white
+
+}
+
+
+
+themes.RoyalGold = {
+
+    name = "Royal Gold",
+
+    background =
+        colors.black,
+
+    primary =
+        colors.yellow,
+
+    secondary =
+        colors.white,
+
+    accent =
+        colors.orange,
+
+    text =
+        colors.white
+
+}
+
+
+
+return themes
