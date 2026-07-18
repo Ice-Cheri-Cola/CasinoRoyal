68b99fb23315546876533be259fb5c3bcb574d20
Ice-Cheri-Cola <alyshamariedel@gmail.com>
2026-07-17 23:27:36 -0400

    Create slots.lua

---
+++ games/slots.lua
@@ -0,0 +1,57 @@
+--================================================--
+-- Casino Royal
+-- Slots Game
+--================================================--
+
+local display =
+    require("core.display")
+
+local ui =
+    require("core.ui")
+
+local slots = {}
+
+
+
+function slots.open()
+
+    display.clear()
+
+    display.center(
+        2,
+        "◇ SLOT MACHINE ◇"
+    )
+
+
+    display.center(
+        5,
+        "💎  7  🍒"
+    )
+
+
+    display.center(
+        8,
+        "GOOD LUCK!"
+    )
+
+
+    ui.button(
+        "PLAY",
+        8,
+        12,
+        8,
+        2,
+
+        function()
+
+            print("Spinning!")
+
+        end
+    )
+
+
+end
+
+
+
+return slots
